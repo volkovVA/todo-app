cdf1350578818e923be0c79d3b4c43f2af5298ff
Volkov Slava <volkov.slava@hotmail.com>
2020-02-05 14:41:14 +0300

    added todo.vue component

---
+++ src/components/todo.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  div.todo
+    todo-input
+    todo-list
+</template>
+
+<script>
+import todoInput from './todoInput';
+import todoList from './todoList';
+
+export default {
+  components: {
+    todoInput,
+    todoList
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .todo {
+    margin-top: 100px;
+    background: #fff;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px rgba(0, 0, 0, 0.1);
+  }
+</style>
