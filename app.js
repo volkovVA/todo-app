d00bfe9d0bdcb7ad2127cc2e5eb20b50629e003e
Volkov Slava <volkov.slava@hotmail.com>
2020-03-17 13:10:29 +0300

    adding a task to DOM

--- app.js
+++ app.js
@@ -30,3 +30,62 @@ const tasks = [
       'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
   },
 ];
+
+(function(arrOfTasks) {
+  const objOfTasks = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+
+  // Elements UI
+
+  const listContainer = document.querySelector(
+    '.tasks-list-section .list-group'
+  );
+
+  renderAllTasks(objOfTasks);
+
+  function renderAllTasks(tasksList) {
+    if(!tasksList) {
+      console.log("Передайте список задач!");
+      return;
+    }
+
+    const fragment = document.createDocumentFragment();
+    Object.values(tasksList).forEach(task => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    });
+    listContainer.appendChild(fragment);
+  }
+
+  function listItemTemplate({_id, title, body} = {}) {
+    const li = document.createElement('li')
+    li.classList.add(
+      'list-group-item',
+      'd-flex',
+      'align-items-center',
+      'flex-wrap',
+      'mt-2'
+    );
+
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.style.fontWeight = 'bold'
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete task';
+    deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn')
+
+    const article = document.createElement('p');
+    article.textContent = body;
+    article.classList.add('mt-2', 'w-100');
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+
+    return li;
+  }
+
+})(tasks);
