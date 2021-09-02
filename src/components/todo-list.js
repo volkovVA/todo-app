6405c3370d538a9549d4cafa5c90d95554bb17db
volkovVA <volkov.slava@hotmail.com>
2021-09-02 21:41:37 +0300

    feat: props in component

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -4,8 +4,11 @@ import TodoListItem from './todo-list-item'
 const TodoList = () => {
   return (
     <ul>
-      <li><TodoListItem /></li>
-      <li><TodoListItem /></li>
+      <li><TodoListItem label="Drink Coffee"/></li>
+      <li><TodoListItem 
+        label="Build React App"
+        important />
+      </li>
     </ul>
   )
 }
