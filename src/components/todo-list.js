e406a0373d2a735f3cd916518b434191fc774780
volkovVA <volkov.slava@hotmail.com>
2021-09-02 22:44:15 +0300

    feat: importing styles

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,25 +1,21 @@
 import React from 'react'
 import TodoListItem from './todo-list-item'
+import './todo-list.css'
 
 const TodoList = ({ todos }) => {
 
   const elements = todos.map((item) => {
-    const { id, ...itemProps } = item
+    const { id, ...itemProps } = item;
 
     return (
-      // <li>
-      //   <TodoListItem 
-      //     label = { item.label }
-      //     important = { item.important } />
-      // </li>
-      <li key = { item.id }>
-        <TodoListItem { ...itemProps } />
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
       </li>
     )
   })
 
   return (
-    <ul>
+    <ul className="list-group todo-list">
       { elements }
     </ul>
   )
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom'
 import AppHeader from './components/app-header'
 import SearchPanel from './components/search-panel'
 import TodoList from './components/todo-list'
+import ItemStatusFilter from './components/item-status-filter'
+import './index.css'
 
 const App = () => {
 
@@ -14,10 +16,14 @@ const App = () => {
   ]
 
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList todos = { todoData }/>
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
     </div>
   )
 }
