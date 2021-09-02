e6c4713ab2b45f34a6a8c3b31e257493f3f4a54f
volkovVA <volkov.slava@hotmail.com>
2021-09-02 21:24:29 +0300

    feat: project structure

--- src/index.js
+++ src/index.js
@@ -1,32 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const TodoList = () => {
-  const items = ['Drink Coffee', 'Build Awesome App']
-
-  return (
-    <ul>
-      <li>{ items[0] }</li>
-      <li>{ items[1] }</li>
-    </ul>
-  )
-}
-
-const AppHeader = () => {
-  return <h1>My Todo List</h1>
-}
-
-const SearchPanel = () => {
-  const searchText = 'Type here to search'
-  const searchStyle = {
-    fontSize: '20px'
-  }
-  
-  return <input 
-    style = { searchStyle }
-    placeholder = { searchText } 
-    className = 'foo' />
-}
+import AppHeader from './components/app-header'
+import SearchPanel from './components/search-panel'
+import TodoList from './components/todo-list'
 
 const App = () => {
   const isLoggedIn = false
