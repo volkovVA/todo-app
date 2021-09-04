c5ddf85985de0968ef4716c0579aa0bcc445dd7a
volkovVA <volkov.slava@hotmail.com>
2021-09-04 16:39:39 +0300

    feat: work with form

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,15 +4,40 @@ import './item-add-form.css'
 
 export default class ItemAddForm extends Component {
 
-  render() {
-    const { onItemAdded } = this.props
+  state = {
+    label: ''
+  }
 
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.onItemAdded(this.state.label)
+    this.setState({
+      label: ''
+    })
+  }
+
+  render() {
     return (
-      <div className="item-add-form">
-      <button
-        className="btn btn-outline-secondary"
-        onClick={() => onItemAdded('Hello World!!!')}>Add Item</button>
-    </div>
+      <form 
+        className="item-add-form d-flex"
+        onSubmit={this.onSubmit}>
+        <input 
+          type="text" 
+          className="form-control"
+          placeholder="What needs to be done"
+          onChange={this.onLabelChange}
+          value={this.state.label}/>
+        <button
+          className="btn btn-outline-secondary">
+            Add Item
+        </button>
+      </form>
     )
   }
 }
